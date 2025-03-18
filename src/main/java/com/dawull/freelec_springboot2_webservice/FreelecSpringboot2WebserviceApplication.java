cce3bed35ee0b37acc336b428a8afb6fcf43fc22
shindawull <juoiy05@naver.com>
2025-03-18 17:52:51 +0900

    Initial commit

---
+++ src/main/java/com/dawull/freelec_springboot2_webservice/FreelecSpringboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.dawull.freelec_springboot2_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringboot2WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawull/freelec_springboot2_webservice/FreelecSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawull.freelec_springboot2_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
